9c95429b8dd9726b9d6abf4b14623f96b0e9d497
Rama Himawan <rama.himawan@mivo.com>
2015-09-29 00:11:11 +0700

    created todo migration.

---
+++ db/migrate/20150929000255_create_todos.rb
@@ -0,0 +1,16 @@
+class CreateTodos < ActiveRecord::Migration
+    def self.up
+        create_table(:todos) do |t|
+            t.datetime(:created_at)
+            t.datetime(:updated_at)
+            t.string(:title)
+            t.text(:detail)
+            t.datetime(:deadline)
+            t.boolean(:is_done)
+        end
+    end
+
+    def self.down
+        drop_table(:todos)
+    end
+end
